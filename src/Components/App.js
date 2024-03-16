7e2b16f53f702efaa73ccb824fa52d8c80d0cb4d
Eric Batiste <ericbatiste4@gmail.com>
2024-03-16 12:39:24 -0600

    Build header.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import data from '../build-data'
 import Articles from './Articles';
 import Details from './Details';
@@ -14,10 +15,17 @@ function App() {
   }, [])
 
   return (
-   <Routes>
-      <Route path="/" element={ <Articles articles={articles} /> } />
-      <Route path="/article/:id" element={ <Details articles={articles} /> } />
-   </Routes> 
+    <>
+      <header>
+        <NavLink to="/" className="app-title">Whoa, News!</NavLink>
+      </header>
+      <main>
+        <Routes>
+            <Route path="/" element={ <Articles articles={articles} /> } />
+            <Route path="/article/:id" element={ <Details articles={articles} /> } />
+        </Routes> 
+      </main>
+    </>
   )
 }
 
